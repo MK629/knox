19a05fa53239ea188fe8d9a4a791c237a07f8793
mk <myatkw629@gmail.com>
2026-02-15 01:47:23 +0630

    more setup

--- lib/db/functions/db_accountant.dart
+++ lib/db/functions/db_accountant.dart
@@ -2,10 +2,19 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbAccountant {
-  static late Database db;
+  static late Database _db;
+
+  static Database get getDb => _db;
 
   static void initDb() async {
     String databasesPath = await getDatabasesPath();
-    db = await openDatabase(join(databasesPath, 'knox.db'));
+
+    _db = await openDatabase(
+      join(databasesPath, 'knox.db'),
+      onCreate: (db, version) {
+        
+      },
+      version: 1
+    );
   }
 }
