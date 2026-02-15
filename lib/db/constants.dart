a2b011d3ea743a26c519b5412865a440c8b0eaca
mk <myatkw629@gmail.com>
2026-02-16 01:43:07 +0630

    write basic db stuff

---
+++ lib/db/constants.dart
@@ -0,0 +1,27 @@
+class Queries {
+  static final String createRecordTable = '''
+
+  ''';
+
+  static final String createConstantIncomeTable = '''
+    
+  ''';
+
+  static final String createConstantExpenseTable = '''
+    
+  ''';
+}
+
+class TableNames{
+  static final String recTbl = "records";
+  static final String conInTbl = "constant_incomes";
+  static final String conOutTbl = "constant_expenses";
+}
+
+enum RecordType{
+  income, expense
+}
+
+enum Interval{
+  hourly, daily, monthly, yearly
+}
--- lib/db/functions/db_accountant.dart
+++ lib/db/functions/db_accountant.dart
@@ -1,10 +1,11 @@
+import 'package:knox/db/constants.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbAccountant {
-  static late Database _db;
+  static late Database? _db;
 
-  static Database get getDb => _db;
+  static Database? get getDb => _db;
 
   static void initDb() async {
     String databasesPath = await getDatabasesPath();
@@ -12,9 +13,27 @@ class DbAccountant {
     _db = await openDatabase(
       join(databasesPath, 'knox.db'),
       onCreate: (db, version) {
-        
+        db.execute(Queries.createRecordTable);
+        db.execute(Queries.createConstantIncomeTable);
+        db.execute(Queries.createConstantExpenseTable);
       },
       version: 1
     );
+  }
+
+  static Future<List<Map<String, Object?>>?> getAllFromTable(String tableName) async {
+    checkIfDbNull(_db);
+    return await _db?.query(tableName);
+  }
+
+  static void deleteFromTable(int id, String tableName) async {
+    checkIfDbNull(_db);
+    await _db?.delete(tableName, where: 'id = ?', whereArgs: [id]);
+  }
+
+  static void checkIfDbNull(Database? db){
+    if(db == null){
+      throw Exception("Database not initialized. Please run DbAccountant.initDb() first.");
+    }
   }
 }
