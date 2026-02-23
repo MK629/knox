215e6fc8a190c09e4072ded457cdc279b8bb7ec4
mk <myatkw629@gmail.com>
2026-02-24 01:13:02 +0630

    refactor

--- lib/db/functions/life_duty_enforcer.dart
+++ lib/db/functions/life_duty_enforcer.dart
@@ -29,37 +29,36 @@ class LifeDutyEnforcer {
     await _enforceMonthly(db);
     await _enforceYearly(db);
   }
-  
+
   //===============[ Private internal ]===============
 
   static Future<void> _enforceDaily(Database? db) async {
+    //search for max date && enforced == true.
+
+    //if null or max != today, proceed
+
+    //if not, increment till today, calc income and expense for each day.
     List<Map<String, Object?>>? lifeDutyResult = await _fetchLifeDutyByInterval(UpdateInterval.daily, db);
 
-    if(lifeDutyResult == null || lifeDutyResult.isEmpty){
-      return;
+    if(lifeDutyResult != null && lifeDutyResult.isNotEmpty){
+      List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
     }
-
-    List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
   }
 
   static Future<void> _enforceMonthly(Database? db) async { 
     List<Map<String, Object?>>? lifeDutyResult = await _fetchLifeDutyByInterval(UpdateInterval.monthly, db);
 
-    if(lifeDutyResult == null || lifeDutyResult.isEmpty){
-      return;
+    if(lifeDutyResult != null && lifeDutyResult.isNotEmpty){
+      List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
     }
-
-    List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
   }
 
   static Future<void> _enforceYearly(Database? db) async {
     List<Map<String, Object?>>? lifeDutyResult = await _fetchLifeDutyByInterval(UpdateInterval.yearly, db);
 
-    if(lifeDutyResult == null || lifeDutyResult.isEmpty){
-      return;
+    if(lifeDutyResult != null && lifeDutyResult.isNotEmpty){
+      List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
     }
-
-    List<LifeDuty> lifeDutyList = lifeDutyResult.map((mapFromDb) => LifeDuty.fromMap(mapFromDb)).toList();
   }
 
   static Future<List<Map<String, Object?>>?> _fetchLifeDutyByInterval(UpdateInterval updateInterval, Database? db) async {
