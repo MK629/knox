964c3604f4bb7be993a1a371e4a580a8e237a785
mk <myatkw629@gmail.com>
2026-03-19 00:31:59 +0630

    Moar stuff

--- lib/app/configs/app_theme.dart
+++ lib/app/configs/app_theme.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme(){
   return ThemeData(
     colorScheme: ColorScheme.light(),
+    appBarTheme: AppBarTheme(
+
+    ),
+    textTheme: TextTheme(
+
+    ),
+    buttonTheme: ButtonThemeData(
+
+    ),
+    iconButtonTheme: IconButtonThemeData(
+
+    ),
     dividerTheme: DividerThemeData(
       color: Colors.grey,
       thickness: 0.75
@@ -13,6 +25,18 @@ ThemeData lightTheme(){
 ThemeData darkTheme(){
   return ThemeData(
     colorScheme: ColorScheme.dark(),
+    appBarTheme: AppBarTheme(
+
+    ),
+    textTheme: TextTheme(
+
+    ),
+    buttonTheme: ButtonThemeData(
+
+    ),
+    iconButtonTheme: IconButtonThemeData(
+
+    ),
     dividerTheme: DividerThemeData(
       color: Colors.grey,
       thickness: 0.75
